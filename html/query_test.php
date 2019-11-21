3835adc61e7f229b9e9a5302ee52ba7788d48368
yanyan7 <sakuraperfection@gmail.com>
2019-11-21 17:07:24 +0900

    create repository

---
+++ html/query_test.php
@@ -0,0 +1,20 @@
+<?php
+
+//DB接続
+try{
+  $pdo = new PDO('mysql:host=localhost;dbname=study_db;charset=utf8', 'sakurai', 'Sakurai@0329');
+} catch(PDOException $e){
+    echo "接続エラー:{$e->getMessage()}";
+  }
+
+//クエリ発行
+foreach($pdo->query('select * from post') as $row){
+  echo "<p>$row[id]: $row[title]: $row[content]: $row[user_id]";
+}
+
+
+
+$pdo = null;
+
+?>
+
---
+++ html/sender.php
@@ -0,0 +1,10 @@
+<!docutype html>
+<html>
+<body>
+        <form id="form" name="form" action="receiver.php" method="POST">
+                <input type="text" id="text" name="text">
+                <input type="submit" id="button" name="button" value="送信">
+        </form>
+</body>
+</html>
+
