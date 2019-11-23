28f4ea93cc8c18202253578b94db59806bdae2ac
yukisakurai <yukisakurai@yukisakurai-no-MacBook-Air.local>
2019-11-23 10:40:44 +0900

    todo make comment input  box

--- html/post/index.php
+++ html/post/index.php
@@ -56,31 +56,31 @@ if( isset($_SESSION['user']) ){
                         <td><label for="user_id">ユーザID</label></td>
                         <td><input type="text" id="user_id" name="user_id" value="<?php echo $user_id = $row['user_id'] ?>"></td>
                     </tr>
+                    <tr>
+                        <td><label for="name">ユーザの名前</label></td>
+                        <td>
+                            <input type="text" id="name" name="name" value="<?php echo $name = $row['name'] ?>"
+                            onclick="submit('user')">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><label for="image">ユーザのイメージ</label></td>
+                        <td><input type="text" id="image" name="image" value="<?php echo $image = $row['image'] ?>"></td>
+                    </tr>
                 </form>
-                <tr>
-                    <td><label for="name">ユーザの名前</label></td>
-                    <td>
-                        <input type="text" id="name" name="name" value="<?php echo $name = $row['name'] ?>"
-                        onclick="submit('user')">
-                    </td>
-                </tr>
-                <tr>
-                    <td><label for="image">ユーザのイメージ</label></td>
-                    <td><input type="text" id="image" name="image" value="<?php echo $image = $row['image'] ?>"></td>
-                </tr>
                 <form id="post" name="post" action="show.php" method="post">
                     <tr>
                         <td><label for="post_id">投稿ID</label></td>
                         <td><input type="text" id="post_id" name="post_id" value="<?php echo $post_id = $row['post_id'] ?>"></td>
                     </tr>
+                    <tr>
+                        <td><label for="title">投稿のタイトル</label></td>
+                        <td>
+                            <input type="text" id="title" name="title" value="<?php echo $title = $row['title'] ?>"
+                            onclick="submit('post')">
+                        </td>
+                    </tr>
                 </form>
-                <tr>
-                    <td><label for="title">投稿のタイトル</label></td>
-                    <td>
-                        <input type="text" id="title" name="title" value="<?php echo $title = $row['title'] ?>"
-                        onclick="submit('post')">
-                    </td>
-                </tr>
                 <tr>
                     <td>--------------------</td>
                     <td>----------------------------------------</td>
--- html/post/show.php
+++ html/post/show.php
@@ -8,40 +8,48 @@ $errorMessage = "";
 
 if( isset($_SESSION['user']) ){
     try{
-        /*
-        ユーザ名、タイトル、本文
-        select
-          u.name
-          ,u.name
-          ,p.content
-        from
-          post p
-        left join
-          user u
-        on
-          p.user_id = u.id
-        where
-          user_id = $_SESSION['user']
-        */
-        
+
+        // 1. 投稿情報取得
         // クエリ発行
         $obj = new connect();
 
-        $sql =  'SELECT ';
-        $sql .=     'u.id as user_id ';
-        $sql .=     ',u.name ';
-        $sql .=     ',c.content ';
-        $sql .= 'FROM ';
-        $sql .=     'comment c ';
-        $sql .= 'LEFT JOIN ';
-        $sql .=     'user u ';
-        $sql .=     'ON ';
-        $sql .=     'c.user_id = u.id ';
-        $sql .= 'WHERE ';
-        $sql .= '   c.post_id = ? ';
+        $sql_p =  'SELECT ';
+        $sql_p .=     'u.id as user_id ';
+        $sql_p .=     ',u.name ';
+        $sql_p .=     ',p.title ';
+        $sql_p .=     ',p.content ';
+        $sql_p .= 'FROM ';
+        $sql_p .=     'post p ';
+        $sql_p .= 'LEFT JOIN ';
+        $sql_p .=     'user u ';
+        $sql_p .=     'ON ';
+        $sql_p .=     'p.user_id = u.id ';
+        $sql_p .= 'WHERE ';
+        $sql_p .= '   p.id = ? ';
 
-        $param = $_POST['post_id'];
-        $stmt = $obj->plural($sql, $param);
+        $param_p = $_POST['post_id'];
+        $stmt_p = $obj->plural($sql_p, $param_p);
+        $row_p = $stmt_p->fetch(PDO::FETCH_ASSOC);
+
+        // 2. コメント情報取得
+        // クエリ発行
+        //$obj = new connect();
+
+        $sql_c =  'SELECT ';
+        $sql_c .=     'u.id as user_id ';
+        $sql_c .=     ',u.name ';
+        $sql_c .=     ',c.content ';
+        $sql_c .= 'FROM ';
+        $sql_c .=     'comment c ';
+        $sql_c .= 'LEFT JOIN ';
+        $sql_c .=     'user u ';
+        $sql_c .=     'ON ';
+        $sql_c .=     'c.user_id = u.id ';
+        $sql_c .= 'WHERE ';
+        $sql_c .= '   c.post_id = ? ';
+
+        $param_c = $_POST['post_id'];
+        $stmt_c = $obj->plural($sql_c, $param_c);
 
     }catch(PDOException $e){
         $errorMessage = 'データベースエラー';
@@ -65,31 +73,60 @@ if( isset($_SESSION['user']) ){
         }
       </script>
     </head>
+
     <body>
-      <?php foreach($rows = $stmt->fetchAll(PDO::FETCH_ASSOC) as $row): ?>
+      <form id="postUser" name="commentUser" action="../user/show.php" method="post">
         <table>
-          <form id="commentUser" name="commentUser" action="../user/show.php" method="post">
-            <tr>
-              <td><label for="name">コメント投稿ユーザID</label></td>
-              <td><input type="text" id="user_id" name="user_id" value="<?php echo $name = $row['user_id'] ?>"></td>
-            </tr>
-          </form>
           <tr>
-            <td><label for="name">コメント投稿ユーザ名</label></td>
+            <td><label for="user_id">投稿ユーザID</label></td>
+            <td><input type="text" id="user_id" name="user_id" value="<?php echo $user_id = $row_p['user_id'] ?>"></td>
+          </tr>
+          <tr>
+            <td><label for="name">投稿ユーザ名</label></td>
             <td>
-              <input type="text" id="name" name="name" value="<?php echo $name = $row['name'] ?>"
-              onclick="submit('commentUser')">
+              <input type="text" id="name" name="name" value="<?php echo $name = $row_p['name'] ?>"
+              onclick="submit('postUser')">
             </td>
           </tr>
           <tr>
-            <td><label for="name">コメント</label></td>
-            <td><input type="text" id="content" name="content" value="<?php echo $name = $row['content'] ?>"></td>
+            <td><label for="title">タイトル</label></td>
+            <td><input type="text" id="title" name="title" value="<?php echo $title = $row_p['title'] ?>"></td>
           </tr>
           <tr>
-            <td>--------------------</td>
-            <td>----------------------------------------</td>
+            <td><label for="content">本文</label></td>
+            <td><input type="text" id="content" name="content" value="<?php echo $content = $row_p['content'] ?>"></td>
+          </tr>
+          <tr>
+            <td>********************</td>
+            <td>****************************************</td>
           </tr>
         </table>
+      </form>
+
+      <?php foreach($rows_c = $stmt_c->fetchAll(PDO::FETCH_ASSOC) as $row_c): ?>
+        <form id="commentUser" name="commentUser" action="../user/show.php" method="post">
+          <table>
+            <tr>
+              <td><label for="user_id">コメント投稿ユーザID</label></td>
+              <td><input type="text" id="user_id" name="user_id" value="<?php echo $user_id = $row_c['user_id'] ?>"></td>
+            </tr>
+            <tr>
+              <td><label for="name">コメント投稿ユーザ名</label></td>
+              <td>
+                <input type="text" id="name" name="name" value="<?php echo $name = $row_c['name'] ?>"
+                onclick="submit('commentUser')">
+              </td>
+            </tr>
+            <tr>
+              <td><label for="content">コメント</label></td>
+              <td><input type="text" id="content" name="content" value="<?php echo $content = $row_c['content'] ?>"></td>
+            </tr>
+            <tr>
+              <td>--------------------</td>
+              <td>----------------------------------------</td>
+            </tr>
+          </table>
+        </form>
       <?php endforeach ?>
     </body>
 
