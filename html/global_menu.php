615ccae80bf68b7a1e9d830c9b1e03af3f8c0c79
yukisakurai <yukisakurai@yukisakurai-no-MacBook-Air.local>
2019-11-26 21:41:43 +0900

    now making global_menu (to use 'include')

---
+++ html/global_menu.php
@@ -0,0 +1,57 @@
+<?php
+
+$webroot = $_SERVER['DOCUMENT_ROOT'];
+
+try{
+    // ユーザ情報取得
+    // クエリ発行
+    $obj = new connect();
+
+    $sql =  'SELECT ';
+    $sql .=     'name ';
+    $sql .= 'FROM ';
+    $sql .=     'user ';
+    $sql .= 'WHERE ';
+    $sql .=     'id = ? ';
+
+    $param = array($_SESSION['user']);
+    $stmt = $obj->plural($sql, $param);
+    $items = $stmt->fetch(PDO::FETCH_ASSOC);
+
+}catch(PDOException $e){
+    $errorMessage = 'データベースエラー';
+    //$errorMessage = $sql;
+    // $e->getMessage() でエラー内容を参照可能（デバッグ時のみ表示）
+    // echo $e->getMessage();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+    </title>
+  </head>
+  <body>
+    <form id="toTop" name="toTop" action="<?php echo $webroot.'/post/index.php' ?>" method="post">
+      <input type="submit" id="top" name="top" value="トップに戻る">
+    </form>
+
+    <form id="logout" name="logout" action="<?php echo $webroot.'/login.php' ?>" method="post">
+      <input type="submit" id="logout" name="logout" value="ログアウト">
+    </form>
+
+    <form id="toPostCreate" name="toPostCreate" action="<?php echo $webroot.'/post/create.php' ?>" method="post">
+      <input type="submit" id="create" name="create" value="投稿する">
+    </form>
+
+    <form id="toUserShow" name="toUserShow" action="<?php echo $webroot.'/user/show.php' ?>" method="post">
+      <input type="hidden" id="user_id" name="user_id" value="<?php echo $_SESSION['user'] ?>">
+      <input type="text" id="name" name="name" value="<?php echo $items['name'] ?>">
+    </form>
+
+
+  </body>
+</html>
